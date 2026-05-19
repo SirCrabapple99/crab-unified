d4848c2c5d50c39f002e44c5f867c563fa1f8387
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-05-19 14:41:47 -0500

    aaaaaaa

--- repo.js
+++ repo.js
@@ -1,4 +1,5 @@
 // main repo adding stuff
+window.repoSettings = {"type": "JSON"};
 
 const repoAdd = document.getElementById("repoMenu");
 function openRepoMenu() {
@@ -7,4 +8,12 @@ function openRepoMenu() {
 
 function closeRepoMenu() {
     repoAdd.classList.remove("open");
+}
+
+function selectRepoType(e) {
+    for (let el of document.getElementsByClassName("repoTypeButton")) {
+         el.classList.remove("selected");
+    }
+
+    e.target.classList.add("selected");
 }
